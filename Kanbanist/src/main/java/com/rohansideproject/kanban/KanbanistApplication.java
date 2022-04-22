960dfe86d6d01e172959c07122ceeaf2b2cc0002
Sil3ntRo <rgayle33614@gmail.com>
2022-04-22 13:45:19 -0400

    adding files

---
+++ Kanbanist/src/main/java/com/rohansideproject/kanban/KanbanistApplication.java
@@ -0,0 +1,13 @@
+package com.rohansideproject.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanistApplication.class, args);
+	}
+
+}
---
+++ Kanbanist/src/test/java/com/rohansideproject/kanban/KanbanistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohansideproject.kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
