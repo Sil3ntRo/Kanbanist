487d64056d9cf5d6258d3660879040c715890c39
Sil3ntRo <rgayle33614@gmail.com>
2022-05-01 11:07:11 -0400

    Added styling to navbar, dashboard, and taskitem

--- kanbanistreact/src/components/Dashboard.js
+++ kanbanistreact/src/components/Dashboard.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import TaskItem from './Task/TaskItem';
 
 class Dashboard extends Component {
   render() {
     return (
-      <h1>Welcome to the Dashboard</h1>
-    )
+
+        <div className="tasks">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4 text-center">Tasks</h1>
+                    <br />
+                    <a href="TaskForm.html" className="btn btn-lg btn-info">
+                        Create a Task
+                    </a>
+                    <br />
+                    <hr />
+                    <TaskItem />
+              
+                </div>
+            </div>
+        </div>
+    </div>
+
+    );
   }
 }
 
