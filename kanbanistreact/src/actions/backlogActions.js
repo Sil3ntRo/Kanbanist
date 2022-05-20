395fb2c237e82ec8345ee921d688d8a76eeaf4bb
Sil3ntRo <rgayle33614@gmail.com>
2022-05-20 12:45:01 -0400

    Added Product Board, Task, Back types & components

--- kanbanistreact/src/actions/backlogActions.js
+++ kanbanistreact/src/actions/backlogActions.js
@@ -1,8 +1,33 @@
 import axios from "axios";
+import { GET_ERRORS, GET_BACKLOG } from "./types";
+
 
 export const addProductTask = (backlog_id, product_task, history) => async dispatch => {
+    try {
+        await axios.post(`/api/backlog/${backlog_id}`, product_task);
+        history.push(`/productBoard/${backlog_id}`);
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        });
+    }
     
-    await axios.post(`/api/backlog/${backlog_id}`, product_task);
-    history.pushState(`/productBoard/${backlog_id}`);
 };
+
+export const getBacklog = backlog_id => async dispatch => {
+    try {
+        const res = await axios.get(`/api/backlog/${backlog_id}`);
+        dispatch({
+            type: GET_BACKLOG,
+            payload: res.data
+        });
+    } catch (err) {
+
+    }
+}
 
