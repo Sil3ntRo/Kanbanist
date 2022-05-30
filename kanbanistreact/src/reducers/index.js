fb21d9a1ed5e69645511bfc42da24009c11f699a
Sil3ntRo <rgayle33614@gmail.com>
2022-05-30 06:46:03 -0400

    Implemented security, login logic, and handled basic routing for expired token

--- kanbanistreact/src/reducers/index.js
+++ kanbanistreact/src/reducers/index.js
@@ -2,9 +2,12 @@ import { combineReducers } from "redux";
 import backlogReducer from "./backlogReducer";
 import errorReducer from "./errorReducer";
 import taskReducer from "./taskReducer";
+import securityReducer from "./securityReducer";
+
 
 export default combineReducers ({
     errors:errorReducer,
     task: taskReducer,
-    backlog: backlogReducer
+    backlog: backlogReducer,
+    security: securityReducer
 });
